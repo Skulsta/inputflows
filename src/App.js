a5d5f2f5574a2bef6a8317eee1a17cc2a79a6784
skulsta <sti.sku@gmail.com>
2021-03-04 13:06:59 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -18,6 +18,29 @@ function App() {
           <Route component={AllPosts} path="/posts" exact />
           <Route component={OnePost} path="/posts/:slug" />
         </Switch>
+        <footer className="bg-gray-800 py-12">
+          <div className="max-w-screen-xl mx-auto">
+            <div className="flex flex-col mx-4 text-white font-thin leading-loose">
+              <h5 className="text-xl font-normal mb-2">Links</h5>
+              <div>
+                <Link to="/posts" className="hover:text-green-200">
+                  Posts
+                </Link>
+              </div>
+              <div>
+                <a
+                  className="hover:text-green-200"
+                  href="https://github.com/Skulsta/inputflows"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  Repository
+                </a>
+              </div>
+            </div>
+          </div>
+        </footer>
       </BrowserRouter>
     </div>
   );
