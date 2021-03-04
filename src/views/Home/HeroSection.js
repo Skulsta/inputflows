e93dc69e9758b538e20d9ba20f1d2cbd742a4a06
skulsta <sti.sku@gmail.com>
2021-03-04 11:15:15 +0100

    SoME icons

--- src/views/Home/HeroSection.js
+++ src/views/Home/HeroSection.js
@@ -1,28 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import TwitterIcon from "../../assets/icons/twitter.svg";
+import LinedInIcon from "../../assets/icons/linkedin.svg";
+import GitHubIcon from "../../assets/icons/github.svg";
 
 export default function HeroSection() {
   return (
     <div className="bg-green-800 bg-opacity-25">
       <div className="justify-center items-center min-h-screen">
         <div className="flex p-4 pt-20 flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 max-w-screen-xl mx-auto">
-          <div className="p-8 border w-full bg-white border-none rounded lg:w-1/2 space-y-8 leading-loose">
-            <h2 className="text-2xl text-center text-gray-700">
-              Stian Skulstad
-            </h2>
-            <div className="flex justify-center space-x-4">
-              <p className="text-center">Twitter</p>
-              <p className="text-center">LinkedIn</p>
-              <p className="text-center">GitHub</p>
+          <div className="p-8 border w-full bg-white border-none rounded lg:w-1/2 leading-loose space-y-8">
+            <div>
+              <h2 className="text-2xl text-center text-gray-800">
+                Stian Skulstad
+              </h2>
+              <p className="text-lg text-center text-gray-600">
+                stian.skulstad@gmail.com
+              </p>
+            </div>
+            <div className="flex justify-center space-x-8">
+              <a
+                href="http://twitter.com/skulsta"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img className="w-8" src={LinedInIcon} alt="linkedin button" />
+              </a>
+              <a
+                href="http://twitter.com/skulsta"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img className="w-8" src={GitHubIcon} alt="linkedin button" />
+              </a>
+              <a
+                href="http://twitter.com/skulsta"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img className="w-8" src={TwitterIcon} alt="twitter button" />
+              </a>
             </div>
           </div>
           <div className="p-8 border bg-white border-none rounded lg:w-2/3 space-y-8">
             <p>
               In the name of open source, the code for this site can be found
-              here.
+              <Link to="/posts" className="text-green-800 hover:text-green-600">
+                {" "}
+                here
+              </Link>
+              .
             </p>
             <p>
-              A section with all kinds of posts (one post) can be accessed here.
-              The data for this section is handled with an implementation of
+              A section with all kinds of posts (one post) can be accessed{" "}
+              <Link to="/posts" className="text-green-800 hover:text-green-600">
+                {" "}
+                here
+              </Link>
+              . The data for this section is handled with an implementation of
               Sanity.
             </p>
             <p>
