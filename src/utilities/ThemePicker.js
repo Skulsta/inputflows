ed4494fb98328f6df84c668351acdb503e098d81
skulsta <sti.sku@gmail.com>
2021-11-05 16:45:13 +0100

    Initial setup for dark theme

---
+++ src/utilities/ThemePicker.js
@@ -0,0 +1,12 @@
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+export const ThemePicker = () => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
